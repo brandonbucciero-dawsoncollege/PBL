7f335494c36872ceb1477af18e0e1eab4aad9ead
brandonbucciero-dawsoncollege <brandon.bucciero@dawsoncollege.qc.ca>
2025-11-19 16:48:04 -0500

    Testing display

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SPI.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_DIN 23
+#define DISPLAY_CLK 18
+#define DISPLAY_CS 5
+#define DISPLAY_DC 16
+#define DISPLAY_RES 17
+
+Adafruit_SSD1306 display(DISPLAY_WIDTH, DISPLAY_HEIGHT, &SPI, DISPLAY_DC, DISPLAY_RES, DISPLAY_CS);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  SPI.begin(DISPLAY_CLK, -1, DISPLAY_DIN, DISPLAY_CS);
+  if (!display.begin(SSD1306_SWITCHCAPVCC)) {
+    for(;;);
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(1);
+  display.setCursor(0, 0);
+  display.println("Hello!");
+  display.display();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop() {}
